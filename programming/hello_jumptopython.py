199ed27d16375d07b0abe0d6fbc2898836514075
kwanhee-seoul <kwanhee04@gmail.com>
2021-07-08 11:06:35 +0900

    using 'for', i could make reverse strings!! :)

--- programming/hello_jumptopython.py
+++ programming/hello_jumptopython.py
@@ -1,2 +1,11 @@
-a = "MacbookPro"
-print(a[4],a[6])
+
+a = "over"
+
+reverse = ''
+
+for char in a:
+    reverse = char + reverse #char와 reverse 의 순서가 가져오는 순서 결정. reverse char라면 순방향.
+
+print(reverse)
+
+
