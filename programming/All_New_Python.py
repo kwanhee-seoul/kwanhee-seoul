bd869b290a9837114c0f1950aa335a9f9593434f
kwanhee-seoul <kwanhee04@gmail.com>
2022-12-01 00:09:13 +0900

    modified:   programming/All_New_Python.py

--- programming/All_New_Python.py
+++ programming/All_New_Python.py
@@ -1,5 +1,11 @@
-T2 = (1,4,5,6 )
 
-T2=T2+(2,3)
+#T2 = (1,4,5,6 )
 
-print(T2)
+#T2=T2+(2,3)
+#print(T2)
+
+aviation={0:"Korean Air", 1:"Asiana Air"}
+
+aviation[1]= "Busan Air"
+
+print(aviation[0])
